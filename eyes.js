70f897d771952ac6e822f8055540b61aa5b8e3b1
Julia Brown <92766600+julbrown@users.noreply.github.com>
2022-01-25 22:20:07 -0500

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const balls = document.getElementsByClassName('ball');
 
 document.onmousemove = (event) => {
@@ -10,17 +9,4 @@ document.onmousemove = (event) => {
     balls[i].style.top = y;
     balls[i].transform = 'translate(-' + x + ',-' + y + ')';
   }
-=======
-const balls = document.getElementsByClassName('ball');
-
-document.onmousemove = (event) => {
-  const x = (event.clientX * 100) / window.innerWidth + '%';
-  const y = (event.clientY * 100) / window.innerHeight + '%';
-
-  for (let i = 0; i < 2; i++) {
-    balls[i].style.left = x;
-    balls[i].style.top = y;
-    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
-  }
->>>>>>> d8ae9ac90e689ef5a5af3cbc2d467cf8da0b2f50
 };
